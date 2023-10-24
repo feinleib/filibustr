3edbd37650f88b7ec16fc331bc70c62523711774
Max Feinleib <mhfeinleib@gmail.com>
2023-10-24 15:03:14 -0500

    Add `chamber` arg to get_voteview_members()

--- R/get_voteview_members.R
+++ R/get_voteview_members.R
@@ -1,7 +1,12 @@
-get_voteview_members <- function(local = TRUE, local_dir = ".") {
+get_voteview_members <- function(local = TRUE, local_dir = ".", chamber = "all") {
   source <- ifelse(local,
-                   paste0(local_dir, "/HSall_members.csv"),
-                   "https://voteview.com/static/data/out/members/HSall_members.csv")
-  readr::read_csv(source,
+                   local_dir,
+                   "https://voteview.com/static/data/out/members")
+  file <- dplyr::case_match(chamber,
+                            c("all", "congress") ~ "HSall_members.csv",
+                            c("house", "h", "hr") ~ "Hall_members.csv",
+                            c("senate", "s", "sen") ~ "Sall_members.csv")
+  full_path <- paste0(source, "/", file)
+  readr::read_csv(full_path,
                   col_types = "ifiinfiiiccnnnnnni")
 }
