55045ec12545f3ef6648cb26bb8441da91453918
Max Feinleib <mhfeinleib@gmail.com>
2023-11-19 18:07:36 -0600

    Test `congress` arg in parties

--- tests/testthat/test-get_voteview_parties.R
+++ tests/testthat/test-get_voteview_parties.R
@@ -6,3 +6,13 @@ test_that("all parties data", {
                c("President", "House", "Senate"))
   expect_equal(unique(all_parties$congress), 1:current_congress())
 })
+
+test_that("filter parties by congress", {
+  parties_99_101 <- get_voteview_parties(congress = 99:101)
+  expect_s3_class(parties_99_101, "tbl_df")
+  expect_length(parties_99_101, 9)
+  expect_equal(nrow(parties_99_101), 15)
+  expect_equal(unique(parties_99_101$congress), 99:101)
+
+  curr_parties <- get_voteview_parties(congress = current_congress())
+})
