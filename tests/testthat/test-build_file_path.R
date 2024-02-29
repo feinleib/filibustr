b0409c2b886a537be9fd313b3fed6ad7a5bcce16
Max Feinleib <mhfeinleib@gmail.com>
2024-02-28 23:14:35 -0600

    Test `build_file_path()` for invalid data sources

--- tests/testthat/test-build_file_path.R
+++ tests/testthat/test-build_file_path.R
@@ -47,6 +47,12 @@ test_that("match congress number", {
   expect_equal(match_congress("all"), "all")
 })
 
+test_that("invalid data sources for `build_file_path()`", {
+  expect_error(build_file_path(), "argument \"data_source\" is missing, with no default")
+  expect_error(build_file_path(data_source = ""), "Invalid data source name: ")
+  expect_error(build_file_path(data_source = "not a source"), "Invalid data source name: not a source")
+})
+
 # Voteview ---------------------------------------
 test_that("`build_voteview_file_path()`: online paths", {
   # sheet_type
