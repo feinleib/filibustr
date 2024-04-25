d12236a25a82c793b47bac1554a9f70400bbb637
Max Feinleib <mhfeinleib@gmail.com>
2024-04-24 23:42:30 -0500

    Comment out slow member-votes tests
    The tests for `get_voteview_member_votes(congress = NULL)` are taking too long for the CHECK process.

--- tests/testthat/test-get_voteview_member_votes.R
+++ tests/testthat/test-get_voteview_member_votes.R
@@ -1,29 +1,29 @@
-test_that("download from Voteview", {
-  online_votes <- get_voteview_member_votes(local = FALSE)
-  expect_s3_class(online_votes, "tbl_df")
-  expect_length(online_votes, 6)
-  expect_equal(levels(online_votes$chamber), c("House", "Senate"))
-  expect_equal(unique(online_votes$congress), 1:current_congress())
-  expect_gte(min(online_votes$prob, na.rm = TRUE), 0)
-  expect_lte(max(online_votes$prob, na.rm = TRUE), 100)
-})
+# test_that("download from Voteview", {
+#   online_votes <- get_voteview_member_votes(local = FALSE)
+#   expect_s3_class(online_votes, "tbl_df")
+#   expect_length(online_votes, 6)
+#   expect_equal(levels(online_votes$chamber), c("House", "Senate"))
+#   expect_equal(unique(online_votes$congress), 1:current_congress())
+#   expect_gte(min(online_votes$prob, na.rm = TRUE), 0)
+#   expect_lte(max(online_votes$prob, na.rm = TRUE), 100)
+# })
 
-test_that("filter votes by chamber", {
-  s_votes <- get_voteview_member_votes(chamber = "s")
-  expect_s3_class(s_votes, "tbl_df")
-  expect_length(s_votes, 6)
-  expect_equal(levels(s_votes$chamber), "Senate")
-  expect_equal(unique(s_votes$congress), 1:current_congress())
-
-  hr_votes <- get_voteview_member_votes(chamber = "hr")
-  expect_s3_class(hr_votes, "tbl_df")
-  expect_length(hr_votes, 6)
-  expect_equal(levels(hr_votes$chamber), "House")
-  expect_equal(unique(hr_votes$congress), 1:current_congress())
-
-  # 4x more members per House vote, so this will hold
-  expect_lt(nrow(s_votes), nrow(hr_votes))
-})
+# test_that("filter votes by chamber", {
+#   s_votes <- get_voteview_member_votes(chamber = "s")
+#   expect_s3_class(s_votes, "tbl_df")
+#   expect_length(s_votes, 6)
+#   expect_equal(levels(s_votes$chamber), "Senate")
+#   expect_equal(unique(s_votes$congress), 1:current_congress())
+#
+#   hr_votes <- get_voteview_member_votes(chamber = "hr")
+#   expect_s3_class(hr_votes, "tbl_df")
+#   expect_length(hr_votes, 6)
+#   expect_equal(levels(hr_votes$chamber), "House")
+#   expect_equal(unique(hr_votes$congress), 1:current_congress())
+#
+#   # 4x more members per House vote, so this will hold
+#   expect_lt(nrow(s_votes), nrow(hr_votes))
+# })
 
 test_that("filter votes by congress", {
   votes_1_5 <- get_voteview_member_votes(congress = 1:5)
