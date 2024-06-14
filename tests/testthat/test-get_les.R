41c5ca3f478dce717c692465de8d2abd95684a4c
Max Feinleib <mhfeinleib@gmail.com>
2024-06-14 13:15:08 -0500

    Test LES column types

--- tests/testthat/test-get_les.R
+++ tests/testthat/test-get_les.R
@@ -53,3 +53,27 @@ test_that("LES 2.0 Senate data", {
   # chamber argument
   expect_equal(s_2, get_les("s", les_2 = TRUE))
 })
+
+test_that("column types", {
+  # Senate, LES Classic
+  s_1 <- get_les("s", les_2 = FALSE)
+  expect_s3_class(s_1, "tbl_df")
+  expect_length(s_1, 60)
+  expect_equal(nrow(s_1), 2533)
+  expect_length(dplyr::select(s_1, dplyr::where(is.integer)), 33)
+  expect_length(dplyr::select(s_1, dplyr::where(is.double)), 9)
+  expect_length(dplyr::select(s_1, dplyr::where(is.factor)), 2)
+  expect_length(dplyr::select(s_1, dplyr::where(is.character)), 4)
+  expect_length(dplyr::select(s_1, dplyr::where(is.logical)), 12)
+
+  # House, LES 2
+  hr_2 <- get_les("hr", les_2 = TRUE)
+  expect_s3_class(hr_2, "tbl_df")
+  expect_length(hr_2, 60)
+  expect_equal(nrow(hr_2), 454)
+  expect_length(dplyr::select(hr_2, dplyr::where(is.integer)), 33)
+  expect_length(dplyr::select(hr_2, dplyr::where(is.double)), 9)
+  expect_length(dplyr::select(hr_2, dplyr::where(is.factor)), 2)
+  expect_length(dplyr::select(hr_2, dplyr::where(is.character)), 3)
+  expect_length(dplyr::select(hr_2, dplyr::where(is.logical)), 13)
+})
