242c6f7e8c74b8b5504a240080c300a68477dba4
Max Feinleib <mhfeinleib@gmail.com>
2024-06-23 13:27:04 -0400

    Test `get_voteview_members()`

--- tests/testthat/test-get_voteview_members.R
+++ tests/testthat/test-get_voteview_members.R
@@ -2,6 +2,8 @@ test_that("download from Voteview", {
   online_voteview_members <- get_voteview_members()
   expect_s3_class(online_voteview_members, "tbl_df")
   expect_length(online_voteview_members, 22)
+  # floor on all-time members length
+  expect_gt(nrow(online_voteview_members), 50400)
   expect_equal(levels(online_voteview_members$chamber),
                c("President", "House", "Senate"))
   expect_equal(unique(online_voteview_members$congress), 1:current_congress())
@@ -65,4 +67,40 @@ test_that("column types", {
   expect_length(dplyr::select(members_98, dplyr::where(is.character)), 2)
   expect_length(dplyr::select(members_98, dplyr::where(is.factor)), 2)
   expect_length(dplyr::select(members_98, dplyr::where(is.logical)), 1)
+})
+
+test_that("local read/write", {
+  # create filepaths
+  tmp_csv <- tempfile(fileext = ".csv")
+  tmp_tsv <- tempfile(fileext = ".tsv")
+  tmp_dta <- tempfile(fileext = ".dta")
+
+  # download data from online
+  all_members_online <- get_voteview_members(chamber = "all")
+  expect_s3_class(all_members_online, "tbl_df")
+  expect_length(all_members_online, 22)
+  expect_gt(nrow(all_members_online), 50400)
+  readr::write_csv(all_members_online, tmp_csv)
+
+  hr_117_online <- get_voteview_members(chamber = "hr", congress = 117)
+  expect_s3_class(hr_117_online, "tbl_df")
+  expect_length(hr_117_online, 22)
+  expect_equal(nrow(hr_117_online), 457)
+  haven::write_dta(hr_117_online, tmp_dta)
+
+  s_70_73_online <- get_voteview_members(chamber = "s", congress = 70:73)
+  expect_s3_class(s_70_73_online, "tbl_df")
+  expect_length(s_70_73_online, 22)
+  expect_equal(nrow(s_70_73_online), 425)
+  readr::write_tsv(s_70_73_online, tmp_tsv)
+
+  # check that local data matches
+  all_members_local <- get_voteview_members(chamber = "all", read_from_local_path = tmp_csv)
+  expect_s3_class(all_members_local, "tbl_df")
+  expect_equal(all_members_local, all_members_online)
+
+  hr_117_local <- get_voteview_members(chamber = "hr", congress = 117,
+                                       read_from_local_path = tmp_dta)
+  expect_s3_class(hr_117_local, "tbl_df")
+  expect_equal(haven::zap_labels(haven::zap_formats(hr_117_local)), hr_117_online)
 })
