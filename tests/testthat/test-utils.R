5d53700f62d041cc8d850d51fd6a7f6b9dc031ee
Max Feinleib <mhfeinleib@gmail.com>
2024-06-24 19:02:00 -0400

    Test `filter_chamber()`

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -22,3 +22,36 @@ test_that("get_online_data(): Voteview parties", {
   expect_equal(unique(parties_df$chamber), c("President", "House", "Senate"))
   expect_equal(unique(parties_df$congress), 1:118)
 })
+
+test_that("filter_chamber()", {
+  # testing with `get_voteview_members()`
+  members_94 <- get_voteview_members(congress = 94)
+  expect_s3_class(members_94, "tbl_df")
+  expect_equal(nrow(members_94), 543)
+
+  # no filter
+  expect_equal(filter_chamber(members_94, chamber = "all"),
+               members_94)
+  expect_equal(filter_chamber(members_94, chamber = "congress"),
+               members_94)
+  expect_equal(filter_chamber(members_94, chamber = "HS"),
+               members_94)
+
+  # filter to Senate
+  sen_94 <- filter_chamber(members_94, chamber = "s")
+  expect_s3_class(sen_94, "tbl_df")
+  expect_equal(nrow(sen_94), 102)
+  expect_equal(sen_94, dplyr::filter(members_94, chamber != "House"))
+
+  expect_equal(filter_chamber(members_94, "senate"), sen_94)
+  expect_equal(filter_chamber(members_94, "sen"), sen_94)
+
+  # filter to House
+  hr_94 <- filter_chamber(members_94, chamber = "hr")
+  expect_s3_class(hr_94, "tbl_df")
+  expect_equal(nrow(hr_94), 442)
+  expect_equal(hr_94, dplyr::filter(members_94, chamber != "Senate"))
+
+  expect_equal(filter_chamber(members_94, "h"), hr_94)
+  expect_equal(filter_chamber(members_94, "house"), hr_94)
+})
