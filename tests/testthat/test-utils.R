6b7eba1de3ff5734185d27d9ebc8579f6677268c
Max Feinleib <mhfeinleib@gmail.com>
2024-07-06 12:55:50 -0400

    Export/doc/test `read_html_table()`

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,3 +1,32 @@
+test_that("read_html_table()", {
+  skip_if_offline()
+
+  # The table used in `get_senate_cloture_votes()`
+  # NOTE: `get_senate_cloture_votes()` performs some cleaning on this table
+  cloture_votes <- read_html_table(
+    url = "https://www.senate.gov/legislative/cloture/clotureCounts.htm",
+    css = ".cloturecount"
+  )
+
+  expect_s3_class(cloture_votes, "tbl_df")
+  expect_length(cloture_votes, 5)
+  # this table will add one row each Congress
+  expect_equal(nrow(cloture_votes), 55 + (current_congress() - 118))
+  expect_equal(colnames(cloture_votes),
+               c("Congress", "Years", "Motions Filed", "Votes on Cloture", "Cloture Invoked"))
+
+
+  # A table from Baseball Reference: Steve Garvey stats
+  br_table <- read_html_table(
+    url = "https://www.baseball-reference.com/players/g/garvest01.shtml",
+    css = "#batting_standard"
+  )
+
+  expect_s3_class(br_table, "tbl_df")
+  expect_length(br_table, 30)
+  expect_equal(nrow(br_table), 27)
+})
+
 test_that("get_online_data(): Voteview members", {
   vv_resp_members_s117 <- get_online_data(
     "https://voteview.com/static/data/out/members/S117_members.csv", "Voteview")
